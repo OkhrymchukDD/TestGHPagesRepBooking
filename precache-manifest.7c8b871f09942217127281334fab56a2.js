375375703968a74f7c76a5be1cdbec536c115874
Dennis <32113099+OkhrymchukDD@users.noreply.github.com>
2020-02-25 17:52:53 +0200

    Add files via upload

---
+++ precache-manifest.7c8b871f09942217127281334fab56a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbcfe24cd1dfb83b6458b6193bb3ff73",
+    "url": "//okhrymchukdd.github.io/TestGHPagesRepBooking/index.html"
+  },
+  {
+    "revision": "f462c661911958736881",
+    "url": "//okhrymchukdd.github.io/TestGHPagesRepBooking/static/css/main.7e480604.chunk.css"
+  },
+  {
+    "revision": "682df117b4590a468f66",
+    "url": "//okhrymchukdd.github.io/TestGHPagesRepBooking/static/js/2.5d11d151.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "//okhrymchukdd.github.io/TestGHPagesRepBooking/static/js/2.5d11d151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f462c661911958736881",
+    "url": "//okhrymchukdd.github.io/TestGHPagesRepBooking/static/js/main.48aaaaca.chunk.js"
+  },
+  {
+    "revision": "d5b376a3184f052c84d2",
+    "url": "//okhrymchukdd.github.io/TestGHPagesRepBooking/static/js/runtime-main.f05f24d9.js"
+  }
+]);
